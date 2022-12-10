99294eb56ed79c29a58a18f7eefe5e5b833065f1
Szymon Rafałowski <szymon2489@gmail.com>
2022-12-10 23:09:12 +0100

    Day 10

---
+++ 2022/blank/blank.js
@@ -0,0 +1,24 @@
+/*
+Description:
+
+
+*/
+
+const fs = require("fs")
+
+fs.readFile("input.txt", (err, input) => {
+    if (err) throw err;
+    let data = input.toString()
+    let lines = data.split('\n')
+
+    lines.forEach(line => {
+        
+    })
+
+    console.log()
+    // 
+})
+
+/*
+
+*/
