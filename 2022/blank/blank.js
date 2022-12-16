2ac595761877c85d065ef5d2b047515ac975b0fe
Szymon Rafałowski <szymon2489@gmail.com>
2022-12-16 18:58:10 +0100

    Day 11 Part 1 (not finished yet)

--- 2022/blank/blank.js
+++ 2022/blank/blank.js
@@ -7,7 +7,7 @@ Description:
 const fs = require("fs")
 
 fs.readFile("input.txt", (err, input) => {
-    if (err) throw err;
+    if (err) throw err
     let data = input.toString()
     let lines = data.split('\n')
 
