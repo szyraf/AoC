8fa00f47bbb10a8bbf40931ae017dfcea98d8e4a
Szymon Rafałowski <szymon2489@gmail.com>
2023-12-03 14:23:52 +0100

    Day 3!

---
+++ 2023/blank/blank.go
@@ -0,0 +1,58 @@
+/*
+Description:
+
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Part 1:")
+	part1()
+	fmt.Println("Part 2:")
+	part2()
+}
+
+func part1() {
+	filePath := "./blank/testinput.txt"
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var fileLines []string
+
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+}
+
+/*
+--- Part Two ---
+
+*/
+
+func part2() {
+	filePath := "./blank/testinput.txt"
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var fileLines []string
+
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+}
